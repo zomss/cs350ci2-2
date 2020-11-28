6799dffe82ed2cfdccb2816ba0b4a7595f3e0baa
hohoho4u4me <63030091+hohoho4u4me@users.noreply.github.com>
2020-11-28 18:14:07 +0900

    changed to match checkstyle

--- src/main/java/simplejavacalculator/SimpleJavaCalculator.java
+++ src/main/java/simplejavacalculator/SimpleJavaCalculator.java
@@ -8,16 +8,13 @@
 
 package simplejavacalculator;
 
-public class SimpleJavaCalculator {
-   
-   public static void main(String[] args) {
+class SimpleJavaCalculator {
+   void main(final String[] args) {
       try {
          UI uiCal = new UI();
          uiCal.init();
+      } catch (Exception e) {
+         System.out.println(e.getMessage());
       }
-      catch (Exception e) {
-         System.out.println(e.getMessage());   
-      }
-      
    }
 }
